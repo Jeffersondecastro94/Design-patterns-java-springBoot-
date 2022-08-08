43fdfd1595f1a11429c4c6e7e1a405350408632e
Jefferson de Castro <Jeffersondecastro94@gmail.com>
2022-08-08 08:52:56 -0300

    padroes de projeto com spring completo

--- lab-padroes-projeto-spring-main/src/main/java/one/digitalinnovation/gof/Application.java
+++ lab-padroes-projeto-spring-main/src/main/java/one/digitalinnovation/gof/Application.java
@@ -1,4 +1,4 @@
-package padroes.padroes.projeto.spring;
+package one.digitalinnovation.gof;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
 @SpringBootApplication
-public class PadroesProjetoSpringApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PadroesProjetoSpringApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
--- lab-padroes-projeto-spring-main/src/test/java/one/digitalinnovation/gof/LabPadroesProjetoSpringApplicationTests.java
+++ lab-padroes-projeto-spring-main/src/test/java/one/digitalinnovation/gof/LabPadroesProjetoSpringApplicationTests.java
@@ -1,10 +1,10 @@
-package padroes.projeto.spring;
+package one.digitalinnovation.gof;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ProjetoSpringApplicationTests {
+class LabPadroesProjetoSpringApplicationTests {
 
 	@Test
 	void contextLoads() {
